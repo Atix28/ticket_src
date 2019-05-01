2146cf4d2c0708f6c048a1f3d72aee7b347ed7fd
Isuru <isuruliyanage1@gmail.com>
2019-05-01 11:32:54 +0530

    sd

--- src/App.js
+++ src/App.js
@@ -1,20 +1,29 @@
-import React from 'react';
+import React  , { Component }from 'react';
 import Main from './Component/Main';
 import './App.css';
 import Nav from './Component/Nav';
+import FeedbackForm from './Component/FeedbackForm';
+import PropTypes from 'prop-types';
 
 
 
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+         
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <FeedbackForm env={this.props.env} />
+      </div>
+    );
+  }
+}
 
-const App = () => (
-  <div>
-    <Nav/>
-    <div className="container">
-           <Main/>
-                               
-    </div>
-  </div>
+App.propTypes = {
+  env: PropTypes.object.isRequired
+};
 
-)
 
 export default App;
